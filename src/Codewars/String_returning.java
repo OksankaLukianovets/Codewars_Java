cfc439aa79ed2bc9a2d7710cd3dce256b632482b
OksankaLukianovets <lukianovets.oksana@gmail.com>
2024-03-02 21:25:16 -0500

    Second commit

--- src/Codewars/String_returning.java
+++ src/Codewars/String_returning.java
@@ -1,9 +1,12 @@
+package Codewars;
+
 public class String_returning {
     public static void main(String[] args) {
         System.out.println(greet("Oksana"));
         System.out.println(greet("Polina"));
         System.out.println(greet("Natalia"));
         System.out.println(greet("Yulia"));
+        System.out.println(greet("Marina"));
     }
 
     public static String greet(String name) {
