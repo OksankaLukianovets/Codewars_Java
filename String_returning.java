9eae391cbbf192a54f0b230ae36001480e2bc7ed
OksankaLukianovets <lukianovets.oksana@gmail.com>
2024-03-02 20:58:14 -0500

    String_returning 2

--- String_returning.java
+++ String_returning.java
@@ -1,11 +1,13 @@
 public class String_returning {
     public static void main(String[] args) {
-               System.out.println(greet("Oksana"));
+        System.out.println(greet("Oksana"));
         System.out.println(greet("Polina"));
         System.out.println(greet("Natalia"));
+        System.out.println(greet("Yulia"));
     }
+
     public static String greet(String name) {
-        return String.format("Hello, " + name +  " how are you doing today?");
+        return String.format("Hello, " + name + " how are you doing today?");
 
     }
 }
