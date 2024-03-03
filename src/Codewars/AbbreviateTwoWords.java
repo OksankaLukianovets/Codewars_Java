ed3ec3e294f43bb431dbe47a3e8d95554e612e37
OksankaLukianovets <lukianovets.oksana@gmail.com>
2024-03-02 21:45:12 -0500

    Second commit

---
+++ src/Codewars/AbbreviateTwoWords.java
@@ -0,0 +1,14 @@
+package Codewars;
+
+public class AbbreviateTwoWords {
+    public static void main(String[] args) {
+        System.out.println(abbrevName("Sam Harris"));
+        System.out.println(abbrevName("patrick feenan"));
+    }
+    public static String abbrevName(String name) {
+        String name1 = name.toUpperCase();
+        String[] words = name1.split(" ");
+        return words[0].charAt(0) + "." + words[1].charAt(0);
+
+    }
+}
