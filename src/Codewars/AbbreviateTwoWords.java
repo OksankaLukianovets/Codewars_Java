55c5ed4356ec2971c57b8eba9d78ea555425a123
OksankaLukianovets <lukianovets.oksana@gmail.com>
2024-03-02 21:46:29 -0500

    AbbreviateTwoWords

--- src/Codewars/AbbreviateTwoWords.java
+++ src/Codewars/AbbreviateTwoWords.java
@@ -4,6 +4,7 @@ public class AbbreviateTwoWords {
     public static void main(String[] args) {
         System.out.println(abbrevName("Sam Harris"));
         System.out.println(abbrevName("patrick feenan"));
+        System.out.println(abbrevName("Patrick peenan"));
     }
     public static String abbrevName(String name) {
         String name1 = name.toUpperCase();
