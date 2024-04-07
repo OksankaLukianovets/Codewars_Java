d6471e10f5e03d75e0242f9ea6e299525d5d8adc
OksankaLukianovets <lukianovets.oksana@gmail.com>
2024-04-06 22:07:58 -0400

    Plural

---
+++ src/Plural.java
@@ -0,0 +1,22 @@
+public class Plural {
+    public static boolean isPlural(float f) {
+//        Version 1
+//        return (f != 1 );
+        //Version 2
+        if (f == 1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPlural(0f)); // true
+        System.out.println(isPlural(0.5f)); // true
+        System.out.println(isPlural(1f)); //false
+        System.out.println(isPlural(100f)); //true
+    }
+
+}
+
+
